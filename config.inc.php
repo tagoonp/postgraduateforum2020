bdd0a81df6ad23585974ebe7ae2357718ff1585d
tagoon <tagoon.p@psu.ac.th>
2020-03-11 19:01:04 +0700

    20200311 18.62

--- config.inc.php
+++ config.inc.php
@@ -11,19 +11,19 @@ date_default_timezone_set("Asia/Bangkok");
 // define( 'TB_PREFIX', 'udix2_' );
 // define( 'ROOT_DOMAIN', 'https://postgraduateform2020.com/' );
 
-// define( 'DB_NAME', 'postgraduateforum2020' );
-// define( 'DB_USER', 'rmis5' );
-// define( 'DB_PASSWORD', 'rmis5' );
-// define( 'DB_HOST', '157.230.46.106' );
-// define( 'TB_PREFIX', 'udix2_' );
-// define( 'ROOT_DOMAIN', 'https://postgraduateform2020.com/' );
-
 define( 'DB_NAME', 'postgraduateforum2020' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'mySsimanh#3' );
-define( 'DB_HOST', 'localhost' );
+define( 'DB_USER', 'rmis5' );
+define( 'DB_PASSWORD', 'rmis5' );
+define( 'DB_HOST', '157.230.46.106' );
 define( 'TB_PREFIX', 'udix2_' );
 define( 'ROOT_DOMAIN', 'https://postgraduateform2020.com/' );
+
+// define( 'DB_NAME', 'postgraduateforum2020' );
+// define( 'DB_USER', 'root' );
+// define( 'DB_PASSWORD', 'mySsimanh#3' );
+// define( 'DB_HOST', 'localhost' );
+// define( 'TB_PREFIX', 'udix2_' );
+// define( 'ROOT_DOMAIN', 'https://postgraduateform2020.com/' );
 
 // Define system parameters
 $sysdate = date('Y-m-d');
